0f9144323f09f74adc92c6ead363a89b5930e05b
Kim Fairbanks <jdfairbanks@alaska.edu>
2018-11-15 16:49:53 -0900

    First commit

---
+++ Assets/scripts/myMeshGeneratorScript.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class myMeshGeneratorScript : MonoBehaviour {
+
+	Mesh mesh;
+	Vector3[] vertices;
+	int[] triangles;
+
+	Vector3 triangleIndex1;
+	Vector3 triangleIndex2; 
+	Vector3 triangleIndex3;
+
+float x, z = 0;
+ public float duration = 1.0F;
+Color pointColor;
+Color oldColor;
+	Vector3 nextTriangleIndex = new Vector3 (1, 0, 1);
+	Color[] myNewColor;
+Material newMaterial;
+	// Use this for initialization
+	void Start () {
+		mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		GetComponent<MeshRenderer>().material = newMaterial;
+		triangleIndex1 = new Vector3 (0,0,0);
+		triangleIndex2 = new Vector3 (0,0,Random.value * 10f);
+		triangleIndex3 = new Vector3 (Random.value * 10f,0,0);
+		pointColor = new Color(x, 0, z);
+		
+		// myNewColor = new Color[]
+		// {
+		// 	pointColor
+		// };
+		CreateShape();
+		UpdateMesh();
+	}
+
+	void CreateShape()
+	{
+		vertices = new Vector3[]
+		{
+			triangleIndex1,
+			triangleIndex2,
+			triangleIndex3
+		};
+
+		triangles = new int[]
+		{
+			0, 1, 2
+		};
+	}
+
+	void UpdateMesh()
+	{
+		mesh.Clear();
+
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+		newMaterial.color =  pointColor;
+
+	}
+
+	private void Update() {
+		
+		oldColor = newMaterial.color;
+		x = Random.value;
+		z = Random.value;
+
+		
+		triangleIndex1 = new Vector3 (0,0,0);
+		triangleIndex2 = new Vector3 (0,0,z);
+		triangleIndex3 = new Vector3 (x,0,0);
+
+         float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        newMaterial.color = Color.Lerp(Color.red, Color.blue, lerp);
+		//newMaterial.color = pointColor;
+		CreateShape();
+		UpdateMesh();
+	}
+
+
+}
---
+++ Assets/scripts/newTerrain.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class newTerrain : MonoBehaviour {
+
+
+	Mesh mesh;
+	Vector3[] vertices;
+	int[] triangles;
+
+	public int xSize = 20;
+	public int zSize = 20;
+	public float waveSpeed = 1;
+	public float factor;
+	// Use this for initialization
+	void Start () {
+		mesh = new Mesh();
+		GetComponent<MeshFilter>().mesh = mesh;
+		factor = Time.deltaTime;
+		// myNewColor = new Color[]
+		// {
+		// 	pointColor
+		// };
+
+		//StartCoroutine(CreateShape());
+		CreateShape();
+	}
+
+	private void Update() {
+		UpdateMesh();
+	}
+
+	//IEnumerator 
+	void CreateShape()
+	{
+		vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+	factor += Time.deltaTime;
+
+	for (int i = 0, z = 0; z <= zSize; z++)
+		{
+			for (int x = 0; x <= xSize; x++)
+			{
+				float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2;
+				vertices[i] = new Vector3(x, y * Mathf.Sin(factor/waveSpeed), z);
+				i++;
+			}
+		}
+
+	triangles = new int[xSize * zSize * 6];
+	int vert = 0;
+	int tris = 0;
+	for (int z = 0; z < zSize; z++)
+	{
+	for (int x = 0; x < xSize; x++)
+	{
+		triangles[tris + 0] = vert + 0;
+		triangles[tris + 1] = vert + xSize + 1;
+		triangles[tris + 2] = vert + 1;
+		triangles[tris + 3] = vert + 1;
+		triangles[tris + 4] = vert + xSize + 1;
+		triangles[tris + 5] = vert + xSize + 2;
+	
+		vert++;
+		tris += 6;
+
+		//yield return new WaitForSeconds(.1f);
+	}
+	vert++;
+	}
+	
+	}
+
+	void UpdateMesh()
+	{
+		mesh.Clear();
+		
+		//for(int i = 0; i < vertices.Length; i ++)
+		//vertices[i] *=  Mathf.Sin(Time.deltaTime);
+		
+		mesh.vertices = vertices;
+		mesh.triangles = triangles;
+
+		mesh.RecalculateNormals();
+		CreateShape();
+
+	}
+
+// 	private void OnDrawGizmos() {
+
+// if (vertices == null)
+// 	return;
+
+// 		for (int i = 0; i < vertices.Length; i++)
+// 		{
+// 			Gizmos.DrawSphere(vertices[i], .1f);
+// 		}
+// 	}
+}
